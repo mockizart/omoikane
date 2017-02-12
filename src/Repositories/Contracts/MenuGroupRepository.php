5aa5cb6963a23ba6484204084345852e545d1704
mockie <rifkimuhammad89@gmail.com>
2017-02-12 14:07:21 +0700

    refator menu group

--- src/Repositories/Contracts/MenuGroupRepository.php
+++ src/Repositories/Contracts/MenuGroupRepository.php
@@ -13,4 +13,49 @@ namespace Omoikane\Repositories\Contracts;
 
 interface MenuGroupRepository {
 
+    /**
+     * Find menu group by id
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function findMenuGroupById($id);
+
+    /**
+     * Add menu group
+     *
+     * @param $name
+     * @return mixed
+     */
+    public function addMenuGroup($name);
+
+    /**
+     * Update menu group
+     *
+     * @param $menuGroupId
+     * @param $name
+     * @return mixed
+     */
+    public function updateMenuGroup($menuGroupId, $name);
+
+    /**
+     * get paginated menu group
+     *
+     * @param $keyword
+     * @param $path
+     * @param $limit
+     * @param $orderBy
+     * @param $order
+     * @return mixed
+     */
+    public function paginateMenuGroup($keyword, $path, $limit, $orderBy, $order);
+
+    /**
+     * Delete menu group
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function deleteMenuGroup($id);
+
 }
